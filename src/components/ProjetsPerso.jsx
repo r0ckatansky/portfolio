f5da09d8d05a09cd2ebfdb5cad5f8376982ef26a
paul trochel <trochel.paul@outlook.fr>
2024-03-04 10:43:40 +0100

    changement dans les projets persos

--- src/components/ProjetsPerso.jsx
+++ src/components/ProjetsPerso.jsx
@@ -14,32 +14,37 @@ import { useRef } from 'react';
 
 
 export default function Projects() {
-  const [mainProjects, setMainProjects] = React.useState([]);
+  const mainProjects = [
+    { id: 1, name: "Un projet de maths", description: "c'est un super projet merci Mr. Campi"},
+    { id: 2, name: "Une SAE", description: "c'est une super SAE merci Mr. Kandi"}
+  ];
   const data = useSelector(selectData);
 
-  React.useEffect(
-    function () {
-      const tempData = [];
-      data.forEach((el, i) => (tempData[i] = Object.create(el)));
-      if (data.length !== 0 && filteredProjects.length !== 0) {
-        const tempArray = tempData.filter((obj) =>
-          filteredProjects.includes(obj.name)
-        );
-        tempArray.length !== 0
-          ? setMainProjects([...tempArray])
-          : setMainProjects([...tempData.slice(0, 3)]);
-      } else {
-        setMainProjects([...tempData.slice(0, 3)]);
-      }
-    },
-    [data]
-  );
+  // React.useEffect(
+  //   function () {
+  //     const tempData = [];
+  //     data.forEach((el, i) => (tempData[i] = Object.create(el)));
+  //     if (data.length !== 0 && filteredProjects.length !== 0) {
+  //       const tempArray = tempData.filter((obj) =>
+  //         filteredProjects.includes(obj.name)
+  //       );
+  //       tempArray.length !== 0
+  //         ? setMainProjects([...tempArray])
+  //         : setMainProjects([...tempData.slice(0, 3)]);
+  //     } else {
+  //       setMainProjects([...tempData.slice(0, 3)]);
+  //     }
+  //   },
+    
+  //   [data]
+  // );
 
   const sectionRef = useRef(null);
 
 
   return (
     <div  ref={sectionRef}>
+      <button onClick={() => console.log(mainProjects)}>click me</button>
       <Element name={"Projets_perso"} id="Projets_perso">
         <section className="section">
           <Container>
@@ -66,8 +71,8 @@ export default function Projects() {
                           image={image}
                           name={name}
                           description={description}
-                          url={html_url}
-                          demo={homepage}
+                          url={null}
+                          demo={null}
                         />
                       </Col>
                     );
